7a822399d856b705d14177a203268d9415c791cb
lilian <l.stoklosa@simplon-charleville.fr>
2019-04-01 16:46:41 +0200

    création pages, navbar et slider

---
+++ index.php
@@ -0,0 +1,31 @@
+<!doctype html>
+  <html>
+    <head>
+      <!-- tags meta-->
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <!-- liens styles css-->
+      <link rel="stylesheet" href="reset.css">
+      <link rel="stylesheet" href="index.css">
+      <title>page type native</title>
+    </head>
+    <body>
+        <!-- création navbar-->
+        <ul class="nav">
+          <li><a href="index.php">Accueil</a></li>
+          <li><a href="news.asp">News</a></li>
+          <li><a href="contact.asp">Contact</a></li>
+          <li style="float:right"><a href="about.asp">A propos</a></li>
+        </ul>
+        <!-- fin navbar-->
+        <!-- slider-->
+        <div class="slider">
+          <div class="slides">
+            <div class="slide"><img src="images/master-sword.jpg"></div>
+            <div class="slide"><img src="images/skull-kid.png"></div>
+            <div class="slide"><img src="images/bongo.jpg"></div>
+            <div class="slide"><img src="images/lich_king.jpg"></div>
+          </div>
+        </div>
+        <!-- fin slider-->
+        <!--image flottant a gauche et txt à droite-->

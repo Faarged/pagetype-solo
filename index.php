7192e589364f0eba5ece9979509c330935f6393b
lilian <l.stoklosa@simplon-charleville.fr>
2019-04-02 15:33:32 +0200

    nouvelles images, slider ok, images flottante, gallerie, footer 1 et 2, début de parallax

--- index.php
+++ index.php
@@ -19,13 +19,125 @@
         </ul>
         <!-- fin navbar-->
         <!-- slider-->
-        <div class="slider">
-          <div class="slides">
-            <div class="slide"><img src="images/master-sword.jpg"></div>
-            <div class="slide"><img src="images/skull-kid.png"></div>
-            <div class="slide"><img src="images/bongo.jpg"></div>
-            <div class="slide"><img src="images/lich_king.jpg"></div>
-          </div>
+        <div id="slider">
+          <figure>
+            <img src="images/master-sword.jpg">
+            <img src="images/skull-kid.png">
+            <img src="images/majora.jpg">
+            <img src="images/lich_king.jpg">
+            <img src="images/master-sword.jpg">
+          </figure>
         </div>
         <!-- fin slider-->
         <!--image flottant a gauche et txt à droite-->
+        <div class="imageflottante">
+          <img src="images/majora.jpg" style="float:left; max-width:50%; height:auto;">
+        </div>
+        <p style="color:green;">Trouver un masque maudit ne va pas faciliter la vie de skull kid, qui va devoir subir les agissements du masque sans pouvoir résister</p>
+        <!-- fin image flottante-->
+        <!-- parallax-->
+        <section id="parallax">
+          <div>J'espère que ça va marcher</div>
+        </section>
+        <!--fin parallax-->
+        <!--gallerie -->
+        <div class="box">
+          <img src="images/lynel.jpeg">
+        </div>
+        <div class="box">
+          <img src="images/lynel.jpeg">
+        </div>
+        <div class="box">
+          <img src="images/lynel.jpeg">
+        </div>
+        <div class="box">
+          <img src="images/lynel.jpeg">
+        </div>
+        <div class="box">
+          <img src="images/lynel.jpeg">
+        </div>
+        <div class="box">
+          <img src="images/lynel.jpeg">
+        </div>
+        <div class="box">
+          <img src="images/lynel.jpeg">
+        </div>
+        <div class="box">
+          <img src="images/lynel.jpeg">
+        </div>
+        <!--fin gallerie-->
+        <!--footer-->
+        <footer>
+          <div id="footer1">
+            <div class="box2">
+
+                  <!-- Links -->
+                  <h5>Navigation</h5>
+
+                  <ul class="list-unstyled">
+                      <li>
+                          <a href="index.php">Accueil</a>
+                      </li>
+                      <li>
+                          <a href="*">News</a>
+                      </li>
+                      <li>
+                          <a href="*">Contact</a>
+                      </li>
+                      <li>
+                          <a href="*">A propos</a>
+                      </li>
+
+                  </ul>
+
+            </div>
+            <div class="box2">
+
+                  <!-- Links -->
+                  <h5>Informations</h5>
+
+                  <ul class="list-unstyled">
+                      <li>
+                          <a href="#!">Qui sommes-nous</a>
+                      </li>
+                      <li>
+                          <a href="#!">Jeux</a>
+                      </li>
+                      <li>
+                          <a href="#!">Préférences</a>
+                      </li>
+                      <li>
+                          <a href="#!">Autres information</a>
+                      </li>
+                  </ul>
+
+            </div>
+            <div class="box2">
+
+                  <!-- Links -->
+                  <h5>CGU et Infos</h5>
+
+                  <ul class="list-unstyled">
+                      <li>
+                          <a href="#!">Conditions d'utilisation</a>
+                      </li>
+                      <li>
+                          <a href="#!">Politique de confidentialité</a>
+                      </li>
+                      <li>
+                          <a href="#!">Aide</a>
+                      </li>
+                      <li>
+                          <a href="#!">Sécurité</a>
+                      </li>
+                  </ul>
+
+            </div>
+          </div>
+          <div id="footer2">
+            <p>2019, tout droits réservés. Créé par Lilian</p>
+            <a href="http://facebook.com/" title="Suivez-moi sur facebook"><img src="images/facebook.svg" /></a>
+            <a href="https://github.com/Faarged?tab=repositories/" title="mon code sur github"><img src="images/github.svg" /></a>
+            <a href="http://twitter.com" title="Suivez-moi sur twitter"><img src="images/twitter.svg" /></a>
+          </div>
+        </footer>
